93fe5ce74eb718e3dee23294da874cc5e8655067
Sh0bhitha <115162144+Sh0bhitha@users.noreply.github.com>
2022-12-27 14:34:02 -0500

    add some code to providers file

--- providers.tf
+++ providers.tf
@@ -1,12 +1,18 @@
 terraform {
-required_providers {
-azurerm = {
-source = "hashicorp/azurerm"
-version = "~> 3.0.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
 }
-}
-required_version = ">= 1.1.0"
-}
+
 provider "azurerm" {
-features {}
+  features {}
+  subscription_id   = "855b3c2f-f781-4828-a396-d5749d577e21"
+  tenant_id       = "ca84daf0-3cd3-4718-bd17-bd6bdb5f1cf9"
+  client_id       = "8a30b0c8-d1f1-4944-b0d4-68d2261a6766"
+  client_secret   = "UcB8Q~Rp3KOTJZfgaP7dQVgkLRsckAYQakjKVceM"
 }
